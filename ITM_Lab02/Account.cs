b5e976ce40e2d63d9675f052f90325c446db89fa
Andreea I <oanaandreeailiuta@gmail.com>
2019-04-03 21:17:57 +0300

    lab 2_update

---
+++ ITM_Lab02/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITM_Lab02
+{
+    class Account
+    {
+        public String UserName { get; set; }
+        public String Address { get; set; }
+        public String Iban { get; set; }
+        public int Amount { get; set; }
+
+        public Account(string user, string address, string iban)
+        {
+            UserName = user;
+            Address = address;
+            Iban = iban;
+            Console.WriteLine("The account for {0}, address: {1}, with IBAN: {2} has been created.", UserName, Address, Iban);
+
+        }
+
+        public void AddDeposit(int sum)
+        {
+            Amount += sum;
+            Console.WriteLine("You added {0} $ into yout account.", sum);
+            Console.WriteLine("Total amount is:  {0} $", Amount);
+
+        }
+
+        public void WitdrawFromDeposit(int sum)
+        {
+            Amount -= sum;
+            Console.WriteLine("You witdrawed {0} $ from yout account.", sum);
+            Console.WriteLine("Remaining amount is:  {0} $", Amount);
+        }
+
+        public void DisplayAmount( string iban)
+        {
+            Console.WriteLine("Your total amount is: {0}" ,Amount);
+        }
+
+    }
+}
---
+++ ITM_Lab02/Bank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITM_Lab02
+{
+    class Bank
+    {
+
+        public string BankName { get; set; }
+        public string BankAddress { get; set; }
+
+        public List<Account> Accounts { get; set; }
+
+        public Bank(string name, string address)
+
+        {
+            BankName = name;
+            BankAddress = address;
+            Accounts = new List<Account>();
+        }
+
+        public void CreatAccount(string accountName, string accoutAddress, string iBan)
+        {
+            var account = new Account(accountName, accoutAddress, iBan);
+            Accounts.Add(account);
+        }
+
+        public Account GetAccount(string iBan)
+        {
+            foreach (var item in Accounts)
+            {
+                if (item.Iban == iBan)
+                {
+                    return item;
+                }
+                
+            }
+            return null;
+        }
+    }
+}
